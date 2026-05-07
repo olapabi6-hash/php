b7802849360a80e1958b18ee73b48854ab34e025
Lola Marsanda <olapabi6@gmail.com>
2026-05-07 14:53:18 +0700

    php

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,13 @@
 <?php
+// Mengizinkan semua origin (untuk development)
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 // PHP akan mencoba mengambil data asli dari Railway dulu, 
 // kalau tidak ada baru pakai data di dalam kutip.
 $host = getenv('MYSQLHOST') ?: "mysql.railway.internal";
 $user = getenv('MYSQLUSER') ?: "root";
-$pass = getenv('MYSQLPASSWORD') ?: "LZstNChgUtUIzpLIpzjVtJVusCMZscPX";
+$pass = getenv('MYSQLPASSWORD') ?: "LOSUiFKXyfvZOlOjXjoQHazazSLRYlOZ";
 $db   = getenv('MYSQLDATABASE') ?: "railway";
 $port = getenv('MYSQLPORT') ?: 3306;
 

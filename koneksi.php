cf1943ac60056f08e3a813b2283e91a90957d97c
Lola Marsanda <olapabi6@gmail.com>
2026-05-07 15:25:24 +0700

    Update MySQL password in koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -9,7 +9,7 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization");
 // kalau tidak ada baru pakai data di dalam kutip.
 $host = getenv('MYSQLHOST') ?: "mysql.railway.internal";
 $user = getenv('MYSQLUSER') ?: "root";
-$pass = getenv('MYSQLPASSWORD') ?: "LOSUiFKXyfvZOlOjXjoQHazazSLRYlOZ";
+$pass = getenv('MYSQLPASSWORD') ?: "mgOEvGrvcuBGNvGBSkpULsNMYyilEOhh";
 $db   = getenv('MYSQLDATABASE') ?: "railway";
 $port = getenv('MYSQLPORT') ?: 3306;
 
